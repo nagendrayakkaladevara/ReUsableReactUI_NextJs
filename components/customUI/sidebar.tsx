4705d3c8b83a65da357d34cdc6906242b6fcf014
nagendrayakkaladevara <nagendrayakkaladevara@gmail.com>
2024-08-26 01:02:59 +0530

    create layout for components page

--- components/customUI/sidebar.tsx
+++ components/customUI/sidebar.tsx
@@ -54,7 +54,7 @@ export function SidebarDemo({ children }: Readonly<{
         <div
             className={cn(
                 "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-                "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
+                "h-screen" 
             )}
         >
             <Sidebar open={open} setOpen={setOpen}>
