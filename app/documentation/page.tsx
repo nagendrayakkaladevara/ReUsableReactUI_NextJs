36a357ea1605a0506d9461f0aed1c8822fcab736
nagendrayakkaladevara <nagendrayakkaladevara@gmail.com>
2024-11-08 09:21:25 +0530

    build fix

--- app/documentation/page.tsx
+++ app/documentation/page.tsx
@@ -29,36 +29,6 @@ const Documentation = () => {
                     <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
                         Next.js Pages Router is a full-stack React framework. Its versatile and lets you create React apps of any size—from a mostly static blog to a complex dynamic application. To create a new Next.js project, run in your terminal:
                     </p>
-                    {/* <div className="grid grid-cols-2 gap-4">
-                        <Image
-                            src="https://assets.aceternity.com/templates/startup-1.webp"
-                            alt="startup template"
-                            width={500}
-                            height={500}
-                            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                        />
-                        <Image
-                            src="https://assets.aceternity.com/templates/startup-2.webp"
-                            alt="startup template"
-                            width={500}
-                            height={500}
-                            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                        />
-                        <Image
-                            src="https://assets.aceternity.com/templates/startup-3.webp"
-                            alt="startup template"
-                            width={500}
-                            height={500}
-                            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                        />
-                        <Image
-                            src="https://assets.aceternity.com/templates/startup-4.webp"
-                            alt="startup template"
-                            width={500}
-                            height={500}
-                            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                        />
-                    </div> */}
                     <div className="border border-input dark:text-white rounded-md p-3 flex justify-between items-center">
                         {textToCopy} <Button variant="outline" className="text-black" onClick={handleCopy}>{copied ? "Copied!" : "Copy"}</Button>
                     </div>
@@ -73,9 +43,9 @@ const Documentation = () => {
                         Installing Tailwind CSS
                     </p>
                     <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-                        If you installed it while creating the app, there's no need to install it again. Otherwise, install it now.
+                        If you installed it while creating the app, there&lsquo;s no need to install it again. Otherwise, install it now.
                     </p>
-                    <Button variant="ghost"><a href="https://tailwindcss.com/docs/installation" target="_blank">Tailwind CSS Doc's</a></Button>
+                    <Button variant="ghost"><a href="https://tailwindcss.com/docs/installation" target="_blank">Tailwind CSS Doc&lsquo;s</a></Button>
                 </div>
             ),
         },
